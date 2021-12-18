9cc406e022b15fd75775e19328ab7724f3837be9
dherv <damien.herv@gmail.com>
2021-12-18 13:19:57 +0900

    feat: init

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,44 @@
+use actix_web::{web, App, HttpServer, Responder, Result};
 use serde::{Deserialize, Serialize};
 use std::fs;
+use uuid::Uuid;
 
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Debug)]
 struct Novel {
+    id: Uuid,
     pages: Vec<Vec<String>>,
 }
 
-impl Novel {
-    fn new(pages: Vec<Vec<String>>) -> Novel {
-        Novel { pages }
-    }
-}
-
-fn run() {
-    let filename = String::from("test");
-    let path = format!("./novels/text/{}.txt", filename);
-    let file = fs::read_to_string(path).expect("file read");
-    let lines: Vec<String> = file
-        .trim()
-        .split("\n")
-        .map(|line| line.to_string())
+async fn get_all() -> Result<impl Responder> {
+    let paths = fs::read_dir("./novels/json").unwrap();
+    let novels: Vec<Novel> = paths
+        .map(|path| fs::read_to_string(path.as_ref().unwrap().path().display().to_string()))
+        .map(|path| serde_json::from_str(&path.unwrap().to_string()).unwrap())
         .collect();
-
-    let pages: Vec<Vec<String>> = lines.chunks(20).map(|chunk| chunk.to_vec()).collect();
-    let result = serde_json::to_string(&Novel::new(pages));
-    match result {
-        Ok(data) => {
-            println!("{}", data);
-            let path = format!("./novels/json/{}.json", filename);
-            fs::create_dir_all("./novels/json").expect("could not create the folder");
-            fs::write(path, data).expect("Unable to write file")
-        }
-        Err(err) => println!("{}", err),
-    }
+    Ok(web::Json(novels))
 }
-fn main() {
-    run()
+
+async fn get_one(id: web::Path<String>) -> Result<impl Responder> {
+    let paths = fs::read_dir("./novels/json").unwrap();
+    let result_path: Result<std::string::String, std::io::Error> = paths
+        .map(|path| path.as_ref().unwrap().path().display().to_string())
+        .filter(|path_string| path_string.contains(&id.to_string()))
+        .map(|path| fs::read_to_string(path))
+        .collect();
+    let novel: Novel = serde_json::from_str(&result_path.unwrap().to_string())?;
+    Ok(web::Json(novel))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(
+            web::scope("/api")
+                .route("", web::get().to(get_all))
+                .route("/{id}", web::get().to(get_one)),
+        )
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
